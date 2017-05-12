8638e2f1bcb9af69701cdbd29dd18b45f2e39ab9
Macklin Underdown <macklinu@users.noreply.github.com>
2017-05-12 12:14:19 -0400

    feat(jiraIssue): add initial implementation (#1)
    `jiraIssue()` is a Danger plugin that adds a JIRA issue link to the Danger pull request comment.
    If a pull request title does not contain the supplied JIRA issue identifier (e.g. ABC-123),
    then Danger will comment with a warning on the pull request asking the developer
    to include the JIRA issue identifier in the pull request title.
    
    Example usage:
    
    ```js
    // dangerfile.js
    import jiraIssue from 'danger-plugin-jira-issue'
    
    jiraIssue({
      key: 'JIRA',
      url: 'https://myjira.atlassian.net/browse',
      emoji: ':paperclip:',
    })
    ```
    
    BREAKING CHANGE: this commit introduces functionality ready for a 1.0.0 release.

---
+++ src/index.test.js
@@ -0,0 +1,84 @@
+import jiraIssue from '../'
+
+describe('jiraIssue()', () => {
+  beforeEach(() => {
+    global.warn = jest.fn()
+    global.message = jest.fn()
+  })
+  afterEach(() => {
+    global.danger = undefined
+    global.warn = undefined
+    global.message = undefined
+  })
+  it('throws when supplied invalid configuration', () => {
+    expect(() => jiraIssue()).toThrow()
+    expect(() => jiraIssue({})).toThrow()
+    expect(() => jiraIssue({ key: 'ABC' })).toThrow()
+    expect(() => jiraIssue({ url: 'http://my.jira/browse' })).toThrow()
+  })
+  it('warns when PR title is missing JIRA issue key', () => {
+    global.danger = { github: { pr: { title: 'Change some things' } } }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse',
+    })
+    expect(global.warn).toHaveBeenCalledWith(
+      'Please add the JIRA issue key to the PR title (e.g. ABC-123)',
+    )
+  })
+  it('adds the JIRA issue link to the messages table', () => {
+    global.danger = {
+      github: { pr: { title: '[ABC-808] Change some things' } },
+    }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse',
+    })
+    expect(global.message).toHaveBeenCalledWith(
+      ':link: <a href="http:/my.jira/browse/ABC-808">ABC-808</a>',
+    )
+  })
+  it('properly concatenates URL parts (trailing slash in url)', () => {
+    global.danger = {
+      github: { pr: { title: '[ABC-808] Change some things' } },
+    }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse/',
+    })
+    expect(global.message).toHaveBeenCalledWith(
+      ':link: <a href="http:/my.jira/browse/ABC-808">ABC-808</a>',
+    )
+  })
+  it('matches JIRA issue anywhere in title', () => {
+    global.danger = { github: { pr: { title: 'My changes - ABC-123' } } }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse',
+    })
+    expect(global.message).toHaveBeenCalledWith(
+      ':link: <a href="http:/my.jira/browse/ABC-123">ABC-123</a>',
+    )
+  })
+  it('does not match lowercase JIRA key in PR title', () => {
+    global.danger = {
+      github: { pr: { title: '[abc-808] Change some things' } },
+    }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse',
+    })
+    expect(global.warn).toHaveBeenCalled()
+  })
+  it('honors custom emoji configuration', () => {
+    global.danger = { github: { pr: { title: '(ABC-123) Change stuff' } } }
+    jiraIssue({
+      key: 'ABC',
+      url: 'http://my.jira/browse',
+      emoji: ':paperclip:',
+    })
+    expect(global.message).toHaveBeenCalledWith(
+      ':paperclip: <a href="http:/my.jira/browse/ABC-123">ABC-123</a>',
+    )
+  })
+})
