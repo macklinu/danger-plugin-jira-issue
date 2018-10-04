851a9b118a853253124e6f2e52cd31ad4a6bab1c
Mackie Underdown <macklinu@users.noreply.github.com>
2018-10-04 09:02:00 -0700

    style(prettier): format codebase with preferred style

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,141 +1,139 @@
-import jiraIssue from "./";
+import jiraIssue from './'
 
-declare const global: any;
+declare const global: any
 
-describe("jiraIssue()", () => {
+describe('jiraIssue()', () => {
   beforeEach(() => {
-    global.warn = jest.fn();
-    global.message = jest.fn();
-  });
+    global.warn = jest.fn()
+    global.message = jest.fn()
+  })
   afterEach(() => {
-    global.danger = undefined;
-    global.warn = undefined;
-    global.message = undefined;
-  });
-  it("throws when supplied invalid configuration", () => {
-    const anyJira = jiraIssue as any;
-    expect(() => anyJira()).toThrow();
-    expect(() => jiraIssue({} as any)).toThrow();
-    expect(() => jiraIssue({ key: "ABC" } as any)).toThrow();
-    expect(() =>
-      jiraIssue({ url: "https://jira.net/browse" } as any)
-    ).toThrow();
-  });
-  it("warns when PR title is missing JIRA issue key", () => {
-    global.danger = { github: { pr: { title: "Change some things" } } };
+    global.danger = undefined
+    global.warn = undefined
+    global.message = undefined
+  })
+  it('throws when supplied invalid configuration', () => {
+    const anyJira = jiraIssue as any
+    expect(() => anyJira()).toThrow()
+    expect(() => jiraIssue({} as any)).toThrow()
+    expect(() => jiraIssue({ key: 'ABC' } as any)).toThrow()
+    expect(() => jiraIssue({ url: 'https://jira.net/browse' } as any)).toThrow()
+  })
+  it('warns when PR title is missing JIRA issue key', () => {
+    global.danger = { github: { pr: { title: 'Change some things' } } }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
     expect(global.warn).toHaveBeenCalledWith(
-      "Please add the JIRA issue key to the PR title (e.g. ABC-123)"
-    );
-  });
-  it("adds the JIRA issue link to the messages table", () => {
+      'Please add the JIRA issue key to the PR title (e.g. ABC-123)'
+    )
+  })
+  it('adds the JIRA issue link to the messages table', () => {
     global.danger = {
-      github: { pr: { title: "[ABC-808] Change some things" } }
-    };
+      github: { pr: { title: '[ABC-808] Change some things' } },
+    }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-808">ABC-808</a>'
-    );
-  });
-  it("properly concatenates URL parts (trailing slash in url)", () => {
+    )
+  })
+  it('properly concatenates URL parts (trailing slash in url)', () => {
     global.danger = {
-      github: { pr: { title: "[ABC-808] Change some things" } }
-    };
+      github: { pr: { title: '[ABC-808] Change some things' } },
+    }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse/"
-    });
+      key: 'ABC',
+      url: 'https://jira.net/browse/',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-808">ABC-808</a>'
-    );
-  });
-  it("matches JIRA issue anywhere in title", () => {
-    global.danger = { github: { pr: { title: "My changes - ABC-123" } } };
+    )
+  })
+  it('matches JIRA issue anywhere in title', () => {
+    global.danger = { github: { pr: { title: 'My changes - ABC-123' } } }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-123">ABC-123</a>'
-    );
-  });
-  it("does not match lowercase JIRA key in PR title", () => {
+    )
+  })
+  it('does not match lowercase JIRA key in PR title', () => {
     global.danger = {
-      github: { pr: { title: "[abc-808] Change some things" } }
-    };
+      github: { pr: { title: '[abc-808] Change some things' } },
+    }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
-    expect(global.warn).toHaveBeenCalled();
-  });
-  it("honors custom emoji configuration", () => {
-    global.danger = { github: { pr: { title: "(ABC-123) Change stuff" } } };
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
+    expect(global.warn).toHaveBeenCalled()
+  })
+  it('honors custom emoji configuration', () => {
+    global.danger = { github: { pr: { title: '(ABC-123) Change stuff' } } }
     jiraIssue({
-      emoji: ":paperclip:",
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
+      emoji: ':paperclip:',
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':paperclip: <a href="https://jira.net/browse/ABC-123">ABC-123</a>'
-    );
-  });
-  it("supports multiple JIRA keys in PR title", () => {
+    )
+  })
+  it('supports multiple JIRA keys in PR title', () => {
     global.danger = {
-      github: { pr: { title: "[ABC-123][ABC-456] Change some things" } }
-    };
+      github: { pr: { title: '[ABC-123][ABC-456] Change some things' } },
+    }
     jiraIssue({
-      key: "ABC",
-      url: "https://jira.net/browse"
-    });
+      key: 'ABC',
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-123">ABC-123</a>, <a href="https://jira.net/browse/ABC-456">ABC-456</a>'
-    );
-  });
-  it("supports multiple JIRA boards in PR title", () => {
+    )
+  })
+  it('supports multiple JIRA boards in PR title', () => {
     global.danger = {
-      github: { pr: { title: "[ABC-123][DEF-456] Change some things" } }
-    };
+      github: { pr: { title: '[ABC-123][DEF-456] Change some things' } },
+    }
     jiraIssue({
-      key: ["ABC", "DEF"],
-      url: "https://jira.net/browse"
-    });
+      key: ['ABC', 'DEF'],
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-123">ABC-123</a>, <a href="https://jira.net/browse/DEF-456">DEF-456</a>'
-    );
-  });
-  it("supports a custom format function", () => {
+    )
+  })
+  it('supports a custom format function', () => {
     global.danger = {
-      github: { pr: { title: "[ABC-123][DEF-456] Change some things" } }
-    };
+      github: { pr: { title: '[ABC-123][DEF-456] Change some things' } },
+    }
     jiraIssue({
       format: (emoji, jiraUrls) => {
-        return `${emoji} JIRA Tickets: ${jiraUrls.join(", ")}`;
+        return `${emoji} JIRA Tickets: ${jiraUrls.join(', ')}`
       },
-      key: ["ABC", "DEF"],
-      url: "https://jira.net/browse"
-    });
+      key: ['ABC', 'DEF'],
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: JIRA Tickets: <a href="https://jira.net/browse/ABC-123">ABC-123</a>, <a href="https://jira.net/browse/DEF-456">DEF-456</a>'
-    );
-  });
-  it("supports JIRA key in the git branch", () => {
+    )
+  })
+  it('supports JIRA key in the git branch', () => {
     global.danger = {
-      github: { pr: { head: { ref: "ABC-808/some-things" } } }
-    };
+      github: { pr: { head: { ref: 'ABC-808/some-things' } } },
+    }
     jiraIssue({
-      key: "ABC",
-      location: "branch",
-      url: "https://jira.net/browse"
-    });
+      key: 'ABC',
+      location: 'branch',
+      url: 'https://jira.net/browse',
+    })
     expect(global.message).toHaveBeenCalledWith(
       ':link: <a href="https://jira.net/browse/ABC-808">ABC-808</a>'
-    );
-  });
-});
+    )
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import { resolve } from "url";
+import { resolve } from 'url'
 
 export interface Options {
   /**
    * The JIRA issue key(s) (e.g. the ABC in ABC-123).
    * Supports multiple JIRA projects (e.g. `['ABC', 'DEF']`).
    */
-  key: string | string[];
+  key: string | string[]
   /** The JIRA instance issue base URL (e.g. https://jira.atlassian.com/browse/). */
-  url: string;
+  url: string
   /**
    * The emoji to display with the JIRA issue link.
    * See the possible emoji values, listed as keys in the
    * [GitHub API `/emojis` response](https://api.github.com/emojis).
    * Defaults to `':link:'`.
    */
-  emoji?: string;
+  emoji?: string
   /**
    * A format function to format the message
    * @param {string} emoji
    * @param {string[]} jiraUrls
    * @returns {string}
    */
-  format?: (emoji: string, jiraUrls: string[]) => string;
+  format?: (emoji: string, jiraUrls: string[]) => string
   /**
    * The location of the JIRA issue, either the PR title, or the git branch
    * Defaults to `title`
    */
-  location?: "title" | "branch";
+  location?: 'title' | 'branch'
 }
 
 const link = (href: string, text: string): string =>
-  `<a href="${href}">${text}</a>`;
+  `<a href="${href}">${text}</a>`
 
 const ensureUrlEndsWithSlash = (url: string) => {
-  if (!url.endsWith("/")) {
-    return url.concat("/");
+  if (!url.endsWith('/')) {
+    return url.concat('/')
   }
-  return url;
-};
+  return url
+}
 
 /**
  * A Danger plugin to add a JIRA issue link to the Danger pull request comment.
@@ -46,57 +46,57 @@ const ensureUrlEndsWithSlash = (url: string) => {
  * to include the JIRA issue identifier in the pull request title.
  */
 export default function jiraIssue(options: Options) {
-  const { key = "", url = "", emoji = ":link:", location = "title" } =
-    options || {};
+  const { key = '', url = '', emoji = ':link:', location = 'title' } =
+    options || {}
   if (!url) {
-    throw Error(`'url' missing - must supply JIRA installation URL`);
+    throw Error(`'url' missing - must supply JIRA installation URL`)
   }
   if (!key) {
-    throw Error(`'key' missing - must supply JIRA issue key`);
+    throw Error(`'key' missing - must supply JIRA issue key`)
   }
 
   // Support multiple JIRA projects.
-  const keys = Array.isArray(key) ? `(${key.join("|")})` : key;
+  const keys = Array.isArray(key) ? `(${key.join('|')})` : key
 
-  const jiraKeyRegex = new RegExp(`(${keys}-[0-9]+)`, "g");
-  let match;
-  const jiraIssues = [];
+  const jiraKeyRegex = new RegExp(`(${keys}-[0-9]+)`, 'g')
+  let match
+  const jiraIssues = []
   // tslint:disable-next-line:no-conditional-assignment
-  let jiraLocation;
+  let jiraLocation
   switch (location) {
-    case "title": {
-      jiraLocation = danger.github.pr.title;
-      break;
+    case 'title': {
+      jiraLocation = danger.github.pr.title
+      break
     }
-    case "branch": {
-      jiraLocation = danger.github.pr.head.ref;
-      break;
+    case 'branch': {
+      jiraLocation = danger.github.pr.head.ref
+      break
     }
     default: {
       throw Error(
         `Invalid value for 'location', must be either "title" or "branch"`
-      );
+      )
     }
   }
-  match = jiraKeyRegex.exec(jiraLocation);
+  match = jiraKeyRegex.exec(jiraLocation)
   while (match != null) {
-    jiraIssues.push(match[0]);
-    match = jiraKeyRegex.exec(jiraLocation);
+    jiraIssues.push(match[0])
+    match = jiraKeyRegex.exec(jiraLocation)
   }
   if (jiraIssues.length > 0) {
     const jiraUrls = jiraIssues.map(issue =>
       link(resolve(ensureUrlEndsWithSlash(url), issue), issue)
-    );
+    )
 
     // use custom formatter, or default
     if (options.format) {
-      message(options.format(emoji, jiraUrls));
+      message(options.format(emoji, jiraUrls))
     } else {
-      message(`${emoji} ${jiraUrls.join(", ")}`);
+      message(`${emoji} ${jiraUrls.join(', ')}`)
     }
   } else {
     warn(
       `Please add the JIRA issue key to the PR ${location} (e.g. ${key}-123)`
-    );
+    )
   }
 }
